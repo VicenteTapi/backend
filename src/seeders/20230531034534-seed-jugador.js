d79279f57e412abb135ab57c6edbe91185309ae8
pascual1 <62040932+pascual1@users.noreply.github.com>
2023-06-01 22:52:38 -0400

    Revert "feat: seeds and models"

--- src/seeders/20230531034534-seed-jugador.js
+++ src/seeders/20230531034534-seed-jugador.js
@@ -15,22 +15,6 @@ module.exports = {
       createdAt: new Date(),
       updatedAt: new Date()
 
-    },
-    {
-      personaje: 'luigi',
-      posicion: 0,
-      monedas: 15,
-      estrellas: 0,
-      poder1: 0,
-      poder2: 0,
-      poder3: 0,
-      dadoGrande: 0,
-      dadoChico: 0,
-      userId: 2,
-      partidaId:1,
-      createdAt: new Date(),
-      updatedAt: new Date()
-
     }
   ]),
   down: (QueryInterface) => QueryInterface.bulkInsert('Jugadors', null, {}),
