f65c14390f4ebfb9994f9c67d0eb8de208e7d0d7
Pascual1 <pascual.seplveda@uc.cl>
2023-06-02 17:33:50 -0400

    fix: se redefinió la tabla tienda

--- src/seeders/20230531034013-seed-tienda.js
+++ src/seeders/20230531034013-seed-tienda.js
@@ -1,36 +1,14 @@
 module.exports = {
   up: (QueryInterface) => QueryInterface.bulkInsert('Tiendas', [
     {
-      item: 'estrella',
-      cantidad: 5,
-      precio: 10,
-      createdAt: new Date(),
-      updatedAt: new Date()
-
-    },
-    {
-      item: 'poder1',
-      cantidad: 5,
-      precio: 20,
-      createdAt: new Date(),
-      updatedAt: new Date()
-
-    },
-    {
-      item: 'dado chico',
-      cantidad: 5,
-      precio: 10,
-      createdAt: new Date(),
-      updatedAt: new Date()
-
-    },
-    {
-      item: 'dado grande',
-      cantidad: 5,
-      precio: 20,
-      createdAt: new Date(),
-      updatedAt: new Date()
-
+    estrellas: 10,  
+    dadoChico: 10,  
+    dadoGrande: 10,  
+    poder1: 10,  
+    poder2: 10,  
+    poder3: 10,
+    createdAt: new Date(),
+    updatedAt: new Date()
     }
   ]),
   down: (QueryInterface) => QueryInterface.bulkInsert('Tiendas', null, {}),
